df3d1d2062777467f671aaf1fa0d76bdf4ddd0c2
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-07-06 09:21:46 +0200

    Using Variables, Types and Enums: Creating and Using Typed Variables

---
+++ src/3-Using-Variables-Types-Enums/index.ts
@@ -0,0 +1,15 @@
+let  firstName: string;
+firstName = 'Dan';
+
+let age: number;
+age = 45;
+
+let hasPurchased = true;
+
+let productName: string[] = ['Basketball', 'Racket', 'Food'];
+productName.push('Swim Glasses');
+
+let petCount: number[] = [];
+petCount.push(5);
+
+console.log(firstName, age, hasPurchased, petCount);
