c046f8452c342e59c77380928788241beffcf906
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-07-07 17:12:53 +0200

    Creating and Using Functions: Specifying Typed Parameters

---
+++ src/4-Using-Functions/output.ts
@@ -0,0 +1,17 @@
+const prefix = '☠️ ';
+
+export default async function updateOutput(id: string) {
+    
+}
+
+runTheLearningSamples();
+
+function runTheLearningSamples(){
+    
+    function displayProductInfo(id: number, name: string){
+        console.log(`${prefix} typed parameters`);
+        console.log(`${prefix}Product ID: ${id}, Name: ${name}`);
+    }
+
+    displayProductInfo(10, 'pizza');
+}
