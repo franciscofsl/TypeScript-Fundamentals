61428be4ad6db7a94b0f3eb379393ea9d90439af
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-07-07 17:33:30 +0200

    Creating and Using Functions: Return Values and Types

--- src/4-Using-Functions/output.ts
+++ src/4-Using-Functions/output.ts
@@ -1,5 +1,11 @@
 const prefix = '☠️ ';
 
+type ProductType = {
+    id: number;
+    name: string;
+    icon?: string;
+}
+
 export default async function updateOutput(id: string) {
     
 }
@@ -18,7 +24,7 @@ function runTheLearningSamples(){
     console.log(`${prefix} function declaration`);
     console.log(addNumbersDeclaration(7, 11));
 
-    function addNumbersDeclaration(a: number, b: number) {
+    function addNumbersDeclaration(a: number, b: number): number {
         const sum: number = a + b;
         return sum;
     }
@@ -35,4 +41,43 @@ function runTheLearningSamples(){
     // A function expression is assigned to a variable, so only the variable is hoisted — not the function itself.
     // Calling a function expression before its definition will cause a runtime error.
 
+    const sampleProducts = [
+        {
+            id: 10,
+            name: 'Pizza slice',
+            icon: 'fas fa-pizza-slice'
+        },
+        {
+            id: 20,
+            name: 'Burger',
+            icon: 'fas fa-hamburger'
+        },
+        {
+            id: 30,
+            name: 'Sushi',
+            icon: 'fas fa-fish'
+        }
+    ];
+
+    function getProductNames(): string[]{
+        return sampleProducts.map(p => p.name);
+    }
+
+    console.log(`${prefix} return array`);
+    console.log(getProductNames());
+
+    function getProductById(id: number): ProductType | undefined {
+        return sampleProducts.find(p => p.id === id);
+    }
+
+    console.log(`${prefix} return ProductType`);
+    console.log(getProductById(20));
+
+    function displayProducts(products: ProductType[]): void {
+        const productNames = products.map(p => p.name.toLocaleLowerCase());
+        const msg = `Sample products include: ${productNames.join(', ')}`;
+        console.log(msg);
+    }
+
+    displayProducts(sampleProducts);
 }
