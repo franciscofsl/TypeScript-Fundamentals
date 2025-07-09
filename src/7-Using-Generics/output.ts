847674a68177ea71f5a27f6e7ab8a5a63a41aa73
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-07-09 20:38:49 +0200

    Creating and Using Generics: Reusable Code and Generics

---
+++ src/7-Using-Generics/output.ts
@@ -0,0 +1,101 @@
+import { productsURL, FoodProduct, customersURL } from '../lib';
+
+const prefix = '🐉 ';
+
+interface HasId {
+  id: number;
+}
+
+class GenericModel<T extends HasId> {
+  public items: T[] | undefined;
+  constructor(public url: string) { }
+
+  async getItems(): Promise<T[]> {
+    this.items = await getList<T>(this.url);
+    return this.items;
+  }
+
+  getItemById(id: number): T | undefined {
+    return this.items ? this.items.find((p) => (id === p.id)) : undefined;
+  }
+}
+
+const foodModel = new GenericModel<FoodProduct>(productsURL);
+
+export default async function updateOutput(id: string = 'output') {
+  // const products = await getProducts();
+  // const products = await getList<FoodProduct>(productsURL);
+  const products = await foodModel.getItems();
+
+  const output = document.querySelector(`#${id}`);
+  const html = layoutProducts(products);
+
+  if (output && html) {
+    output.innerHTML = html;
+  }
+}
+
+function layoutProducts(products: FoodProduct[]): string {
+  const items = products.map(({ id, name, icon }) => {
+    const productHtml = `
+    <span class="card-id">#${id}</span>
+      <i class="card-icon ${icon} fa-lg"></i>
+    <span class="card-name">${name}</span>
+    `;
+    const cardHtml = `
+    <li>
+        <div class="card">
+            <div class="card-content">
+                <div class="content">
+                ${productHtml}
+                </div>
+            </div>
+        </div>
+    </li>
+    `;
+    return cardHtml;
+  });
+  let productsHtml = `<ul>${items.join('')}</ul>`;
+  return productsHtml;
+}
+
+async function getProducts(): Promise<FoodProduct[]> {
+  const response: Response = await fetch(productsURL);
+  const products: FoodProduct[] = await response.json();
+  return products;
+}
+
+async function getList<T>(url: string): Promise<T[]> {
+  const response: Response = await fetch(url);
+  const items: T[] = await response.json();
+  return items;
+}
+
+/************************************************
+ * Learning sample code.
+ ***********************************************/
+
+runTheLearningSamples();
+
+async function runTheLearningSamples() {
+  function whatIsIt_number(arg: number): number {
+    return arg;
+  }
+
+  console.log(prefix + ' Generics Overview');
+  console.log(whatIsIt_number(42)); 
+
+   function whatIsIt_string(arg: string): string {
+    return arg;
+  }
+  console.log(whatIsIt_string("String value")); 
+
+  function whatIsIt_typed<T>(arg: T): T {
+    return arg;
+  }
+
+  let n = whatIsIt_typed<number>(56);
+  let s: string = whatIsIt_typed<string>("Hello, TypeScript!");
+  let b: boolean = whatIsIt_typed<boolean>(true);
+  console.log(n,s,b);
+}
